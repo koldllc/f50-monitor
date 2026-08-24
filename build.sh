95cf78aedd4cd402228c8a68d5fcc1336c5108e6
kelvinsze <me@kelvinsze.com>
2026-08-24 12:30:58 +0800

    Beta 安装被稳定版覆盖 | 提升本地测试构建版本标识

--- build.sh
+++ build.sh
@@ -37,9 +37,9 @@ cat << 'EOF' > "$APP_DIR/Contents/Info.plist"
     <key>CFBundlePackageType</key>
     <string>APPL</string>
     <key>CFBundleShortVersionString</key>
-    <string>2.2.1</string>
+    <string>2.3.1</string>
     <key>CFBundleVersion</key>
-    <string>2.2.1</string>
+    <string>2.3.1b1</string>
     <key>LSMinimumSystemVersion</key>
     <string>13.0</string>
     <key>LSUIElement</key>
